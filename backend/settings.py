6ae1c43622a4713ac04e430d595796be6375ca89
Orkney06 <kome8728@gmail.com>
2022-04-11 22:23:00 +0900

    change requirements.txt

--- backend/settings.py
+++ backend/settings.py
@@ -2,6 +2,8 @@
 import django_heroku
 from pathlib import Path
 import os
+import dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
